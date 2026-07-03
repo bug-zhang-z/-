86dcfe571e398a9e004c891d8d4d6b0b850be492
bug <q13949649392@outlook.com>
2026-07-04 00:02:05 +0800

    Update src/config/navBarConfig.ts (via Pages CMS)

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -81,12 +81,6 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 				icon: "fa7-brands:github",
 			},
 			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: false,
-				icon: "fa7-brands:gitee",
-			},
-			{
 				name: "QQ交流群",
 				url: "https://qm.qq.com/q/ZGsFa8qX2G",
 				external: false,
